c0ad618fee5a91f2d42953688434ebd8c3673eea
Muhammadirfan68 <muhammadirfan01584@gmail.com>
2023-10-30 04:39:58 -0700

    update header
    Categories modified to Search Result and extra code removed

--- include/header.php
+++ include/header.php
@@ -11,25 +11,7 @@
         <ul>
           <li><a href="index.php">Blog</a></li>
           <li><a href="single-post.php">Single Post</a></li>
-          <li class="dropdown"><a href="category.php"><span>Categories</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="search-result.php">Search Result</a></li>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-
+          <li class="dropdown"><a href="category.php">Search Result</a></li>
           <li><a href="about.php">About</a></li>
           <li><a href="contact.php">Contact</a></li>
         </ul>
